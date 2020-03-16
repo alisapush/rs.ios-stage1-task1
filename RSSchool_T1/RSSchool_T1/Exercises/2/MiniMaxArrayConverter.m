e7a0848e438ca8134bf8c2cdf51f672ada94d301
Алиса Пушнова <alisa.pushnova@gmail.com>
2020-03-16 19:58:09 +0300

    feat: task 2 implementation

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -2,9 +2,20 @@
 
 @implementation MiniMaxArrayConverter
 
-// Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSSortDescriptor *highestToLowest =
+    [NSSortDescriptor sortDescriptorWithKey:(@"self") ascending:(NO)];
+    NSMutableArray *sortedArray = [array mutableCopy];
+    [sortedArray sortUsingDescriptors:[NSArray arrayWithObject:highestToLowest]];
+    
+    int max = 0, min = 0;
+    for (int i = 0, j = (int)[sortedArray count] - 1; i < (int)[sortedArray count] - 1; i++, j--) {
+        max += [[sortedArray objectAtIndex:i] intValue];
+        min += [[sortedArray objectAtIndex:j] intValue];
+    }
+    
+    return @[[NSNumber numberWithInt:min], [NSNumber numberWithInt:max]];
 }
 
 @end

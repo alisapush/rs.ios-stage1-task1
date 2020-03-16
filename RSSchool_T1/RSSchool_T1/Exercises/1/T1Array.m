18502e6c69b9ad74bd44e6dfc63d4800d876d1fd
Алиса Пушнова <alisa.pushnova@gmail.com>
2020-03-16 19:57:30 +0300

    feat: task 1 implementation

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -2,9 +2,23 @@
 
 @implementation T1Array
 
-// Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *resultArray = [sadArray mutableCopy];
+    
+    for (int i = 1; i < (int)[resultArray count] - 1; i++) {
+        NSNumber *prev = i - 1 >= 0 ? [resultArray objectAtIndex:i - 1] : [NSNumber numberWithInt:0];
+        NSNumber *next = i + 1 < [resultArray count] ? [resultArray objectAtIndex:i + 1] : [NSNumber numberWithInt:0];
+        NSNumber *current = [resultArray objectAtIndex:i];
+        
+        if (prev.intValue + next.intValue < current.intValue) {
+            [resultArray removeObjectAtIndex:i];
+            i -= 2;
+        }
+    }
+    
+    return resultArray;
 }
+
+
 
 @end
